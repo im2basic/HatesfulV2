5ece71a7d0a8c0d4e373973612dd6f4f5d02fc2f
tata <basically.tata@gmail.com>
2022-10-23 04:58:24 -0500

    #Hatesful-2 Added Get methods for products

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -30,5 +30,16 @@ namespace API.Controllers
         {
             return await _repo.GetProductByIdAsync(id);
         }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _repo.GetProductBrandsAsync());
+        }
+        [HttpGet("type")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _repo.GetProductTypesAsync());
+        }
     }
 }
